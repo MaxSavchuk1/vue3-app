4bda6242db94c4f4352f0bcf8d0f4a99ed4081a9
Maksym Savchuk <m.savchuk@urancompany.com>
2024-10-14 13:11:56 +0300

    element ui integration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import './styles/index.scss'
+import 'remixicon/fonts/remixicon.css'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
-import './styles/index.scss'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
