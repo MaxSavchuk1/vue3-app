67aee864c14756ce1d7766100145c383d4847ed7
Maksym Savchuk <m.savchuk@urancompany.com>
2024-10-14 15:48:53 +0300

    add pinia to the app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
 import './styles/index.scss'
 import 'remixicon/fonts/remixicon.css'
 import 'element-plus/dist/index.css'
-import App from './App.vue'
-import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+app.use(pinia)
 app.mount('#app')
