f2647aaefd732e3ce01822cd98be8883000072bb
Maksym Savchuk <m.savchuk@urancompany.com>
2024-10-16 18:17:36 +0300

    add 'scroll to top' button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,12 +2,25 @@
 const { isLoading } = storeToRefs(useLoaderStore())
 const route = useRoute()
 
+const target = ref(null)
+const targetIsVisible = ref(false)
+
 const isHomePage = computed(() => {
   return route.path === '/'
+})
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+useIntersectionObserver(target, ([{ isIntersecting }]) => {
+  targetIsVisible.value = isIntersecting
 })
 </script>
 
 <template>
+  <div id="scroll-plug" ref="target"></div>
+
   <SideBar />
 
   <main class="main-container">
@@ -21,5 +34,14 @@ const isHomePage = computed(() => {
     <div v-if="isLoading" class="loader-wrapper">
       <div class="loader"></div>
     </div>
+    <transition name="slide">
+      <div
+        v-if="!targetIsVisible && !isLoading"
+        class="scroll-to-top-button"
+        @click="scrollToTop"
+      >
+        <i class="ri-arrow-up-line"></i>
+      </div>
+    </transition>
   </teleport>
 </template>
